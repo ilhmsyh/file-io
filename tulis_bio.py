e92f16d756b877dcf3b6625766a5fc376f5f721f
ilhmsyh <ilhamsyah78607@gmail.com>
2024-07-24 11:11:15 +0700

    halo

--- tulis_bio.py
+++
@@ -1,9 +0,0 @@
-print ("Selamat Datang di Program Biodata")
-print ("==================================")
-
-nama = raw_input("nama: ")
-umur = input("umur: ")
-alamat = raw_input("alamat ")
-
-teks = ("nama: {}\nUmur: {}\nAlamat:: {}".format(nama, umur, alamat))
-file_bio = open("biodata")
